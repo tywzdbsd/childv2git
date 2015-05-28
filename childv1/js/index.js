314eaeb8b36689d91c62bcde7de5f31fd17a825f
tywzdbsd <370464169@qq.com>
2015-05-28 15:57:23 +0800

    v3make
    bb

---
+++ childv1/js/index.js
@@ -0,0 +1,10 @@
+$(function(){
+//绑定click事件
+    $(".detailIcon").on("click",function(){
+        $(".detail").css("display","block");
+        $(".detail .logo-2").addClass("z-index110").css({"left":"15%","top":"5%","width":"70%"});
+    });
+});
+//left:11%;
+//top:20%;
+//width:78%;
